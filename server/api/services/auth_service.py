3ea6318235405b284227033af15961799d95348f
schweigenderFlugel <facundoezcastro@gmail.com>
2025-08-01 17:52:37 -0300

    chore: erase unnecessary endpoints

--- server/api/services/auth_service.py
+++ server/api/services/auth_service.py
@@ -27,24 +27,3 @@ def login(user: Login, db: SessionDep):
 
     access_token = create_access_token(data={"sub": db_user.email, "role": db_user.role})
     return {"access_token": access_token, "token_type": "bearer"}
-
-def get_current_user(token: str, db: SessionDep):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email: str = payload.get("sub")
-        if email is None:
-            raise credentials_exception
-    except JWTError:
-        raise credentials_exception
-
-    user = db.query(User).filter(User.email == email).first()
-    if user is None:
-        raise credentials_exception
-
-    return user
