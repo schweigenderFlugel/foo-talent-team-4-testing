4a7571567a866f5cd986969b3b7accc167111922
Maidana <franco_maidana07@hotmail.com>
2025-08-01 17:32:14 -0300

    client: style: se agrego un fondo en login y register

--- client/src/app/(auth)/register/page.tsx
+++ client/src/app/(auth)/register/page.tsx
@@ -10,9 +10,9 @@ const RegisterPage = () => {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="bg-muted relative hidden lg:block">
         <img
-          src="/placeholder.svg"
+          src="https://wallpaperbat.com/img/853516-marvel-comic-characters-wallpaper-top-free-marvel-comic-characters-background.jpg"
           alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="absolute inset-0 h-full w-full object-cover object-left brightness-[0.9]"
         />
       </div>
       <div className="flex flex-col gap-4 p-6 md:p-10">
